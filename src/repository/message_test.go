19608faa16ab43f4db54b6f3bff8930929baaf45
platylm <n_nareenart@kkumail.com>
2019-08-28 15:50:21 +0700

    [Edited]: ปรับ test ให้เป็น integration test ได้ ที่ src/repository/message_test.go

--- src/repository/message_test.go
+++ src/repository/message_test.go
@@ -1,32 +1,59 @@
-package repository
+// +build integration
+
+package repository_test
 
 import (
 	"greeting/model"
+	. "greeting/repository"
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_GetMessage_Should_Be_ID_1_Message_HelloWorld(t *testing.T) {
-	dbConfig := Config{
-		Host:     "localhost",
-		Port:     5432,
-		User:     "greeting",
-		Password: "greeting",
-		SSLMode:  "disable",
-		DBName:   "greeting",
-	}
-	connectionDB, _ := dbConfig.Open()
-	db := PostgresDB{
-		DB: connectionDB,
-	}
-	expected := model.Greeting{
-		ID:      1,
-		Message: "HelloWorld",
+func Test_ProductDB(t *testing.T) {
+	config := Config{
+		Host:       "localhost",
+		Port:       5432,
+		User:       "greeting",
+		Password:   "greeting",
+		DBName:     "greeting",
+		DisableTLS: true,
 	}
 
-	actual, _ := db.GetMessage()
+	db, err := Open(config)
+	if err != nil {
+		log.Fatal("connect db error: ", err)
+	}
+	defer db.Close()
 
-	assert.Equal(t, expected, actual)
+	schema := `
+	CREATE TABLE greeting (
+		id   int,
+		message TEXT,
+	
+		PRIMARY KEY (id)
+	);
+	`
+	resual, err := NewTable(db, schema)
+	if err != nil {
+		t.Log("ceate tabel error: ", err)
 
+	}
+	t.Log(resual)
+	postgresDB := PostgresDB{
+		DB: db,
+	}
+
+	t.Run("Get Message By ID", func(t *testing.T) {
+		expected := model.Greeting{
+			ID:      1,
+			Message: "Hello World",
+		}
+
+		actual, err := postgresDB.GetMessage()
+		assert.Equal(t, nil, err)
+		assert.Equal(t, expected, actual)
+	})
+	t.Log(resual)
 }
