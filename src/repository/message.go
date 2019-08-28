6d40befdc3fd4a38c4fb9338a7159c10125af6b5
platylm <n_nareenart@kkumail.com>
2019-08-28 15:48:42 +0700

    [Edited]:ย้าย Config มาใส่ใน database.go แทน

--- src/repository/message.go
+++ src/repository/message.go
@@ -1,39 +1,17 @@
 package repository
 
 import (
-	"fmt"
 	"greeting/model"
 
 	"github.com/jmoiron/sqlx"
 )
 
-const (
-	host     = "localhost"
-	port     = 3306
-	user     = "greeting"
-	password = "greeting"
-	dbname   = "greeting"
-)
-
-type Config struct {
-	Host     string
-	Port     int
-	User     string
-	Password string
-	SSLMode  string
-	DBName   string
+type MessageDB interface {
+	GetMessage() (model.Greeting, error)
 }
 
 type PostgresDB struct {
 	DB *sqlx.DB
-}
-
-func (cfg Config) Open() (*sqlx.DB, error) {
-	psqlInfo := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode)
-
-	return sqlx.Connect("postgress", psqlInfo)
 }
 
 func (postgress PostgresDB) GetMessage() (model.Greeting, error) {
