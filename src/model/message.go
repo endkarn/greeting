8be86987241123bc01f2502ced7546b0214212a6
platylm <n_nareenart@kkumail.com>
2019-08-29 15:31:52 +0700

    [Created]:struct name NewGreeting ที่ src/model/message.go

--- src/model/message.go
+++ src/model/message.go
@@ -4,3 +4,8 @@ type Greeting struct {
 	ID      int    `json:"id,omitempty" sql:"id"`
 	Message string `json:"message" sql:"message"`
 }
+
+type NewGreeting struct {
+	ID      int    `json:"id,omitempty"`
+	Message string `json:"message"`
+}
