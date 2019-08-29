922b7fb15b02d9ae513c8f526390bd5391a92419
platylm <n_nareenart@kkumail.com>
2019-08-29 15:32:23 +0700

    [Created]:function and unit test ของ InsertMessage ที่ src/repository/message_test.go

--- src/repository/message.go
+++ src/repository/message.go
@@ -14,10 +14,24 @@ type PostgresDB struct {
 	DB *sqlx.DB
 }
 
-func (postgress PostgresDB) GetMessage() (model.Greeting, error) {
+func (postgres PostgresDB) InsertMessage(newGreeting model.NewGreeting) (model.Greeting, error) {
+	greeting := model.Greeting{
+		ID:      1,
+		Message: "Hello World",
+	}
+	const query = `INSERT INTO greeting (id, "message") VALUES ($1, $2)`
+	tx := postgres.DB.MustBegin()
+	tx.MustExec(query, greeting.ID, greeting.Message)
+	if err := tx.Commit(); err != nil {
+		return model.Greeting{}, err
+	}
+	return greeting, nil
+}
+
+func (postgres PostgresDB) GetMessage() (model.Greeting, error) {
 	var greeting model.Greeting
 	const query = `SELECT id,message FROM greeting WHERE id = 1`
-	err := postgress.DB.Get(&greeting, query)
+	err := postgres.DB.Get(&greeting, query)
 	if err != nil {
 		return model.Greeting{}, err
 	}
--- src/repository/message_test.go
+++ src/repository/message_test.go
@@ -25,9 +25,40 @@ func Test_ProductDB(t *testing.T) {
 	}
 	defer db.Close()
 
+	schema := `
+		CREATE TABLE greeting (
+			id           INTEGER,
+			message         TEXT,
+
+			PRIMARY KEY (id)
+	   );
+		`
+	resual, err := NewTable(db, schema)
+	if err != nil {
+		t.Log("create tabel error: ", err)
+
+	}
+	t.Log(resual)
+
 	postgresDB := PostgresDB{
 		DB: db,
 	}
+
+	t.Run("Insert Message Hello World", func(t *testing.T) {
+		expected := model.Greeting{
+			ID:      1,
+			Message: "Hello World",
+		}
+
+		newMessage := model.NewGreeting{
+			ID:      1,
+			Message: "Hello World",
+		}
+
+		actual, err := postgresDB.InsertMessage(newMessage)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, expected, actual)
+	})
 
 	t.Run("Get Message By ID", func(t *testing.T) {
 		expected := model.Greeting{
@@ -40,7 +71,7 @@ func Test_ProductDB(t *testing.T) {
 		assert.Equal(t, expected, actual)
 	})
 
-	resual, err := DropTable(db, "greeting")
+	resual, err = DropTable(db, "greeting")
 	if err != nil {
 		t.Log("drop tabel error: ", err)
 
